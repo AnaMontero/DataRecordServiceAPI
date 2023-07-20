79d35fb903356d3cd2064a1ee959bb28796ae0b9
Ana Montero <anamongar@gmail.com>
2023-07-20 23:13:02 +0200

    Set up project

---
+++ src/main/java/com/keyfactor/datarecordserviceapi/DataRecordServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.keyfactor.datarecordserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRecordServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRecordServiceApiApplication.class, args);
+	}
+
+}
